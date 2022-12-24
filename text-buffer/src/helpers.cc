7ad9850854b34bbd72735347184d17e7094d2617
Elias Aebi <eyelash@users.noreply.github.com>
2022-12-24 08:18:54 +0100

    fix extentForText

--- text-buffer/src/helpers.cc
+++ text-buffer/src/helpers.cc
@@ -7,9 +7,9 @@ Point extentForText(const std::u16string &text) {
   double row = 0;
   Regex::MatchData match_data(LF_REGEX);
   Regex::MatchResult result = LF_REGEX.match(text.data() + lastLineStartIndex, text.size() - lastLineStartIndex, match_data);
-  while (result.type == Regex::MatchResult::Partial || result.type == Regex::MatchResult::Full) {
+  while (result.type == Regex::MatchResult::Full) {
     row++;
-    lastLineStartIndex = result.end_offset;
+    lastLineStartIndex += result.end_offset;
     result = LF_REGEX.match(text.data() + lastLineStartIndex, text.size() - lastLineStartIndex, match_data);
   }
   return Point(row, text.size() - lastLineStartIndex);
